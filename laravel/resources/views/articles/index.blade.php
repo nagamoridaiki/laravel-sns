fa6da8b479f66a3951698565e983eb0cc64ff50b
nagamoridaiki <no.swea1017@gmail.com>
2020-05-10 17:22:23 +0900

    NAVIバー修正とタグのサイドバーの作成

--- laravel/resources/views/articles/index.blade.php
+++ laravel/resources/views/articles/index.blade.php
@@ -10,12 +10,17 @@
             {{ session('flash_message') }}
         </div>
     @endif
-    <div class="col-md-2 col-xs-12">
-      @include('articles.tags')
+    <div class="row">
+      <div class="col-2 col-xs-12">
+        <div class="sidebar_fixed">
+          @include('articles.tags')
+        </div>
+      </div>
+      <div class="col-9 col-xs-12">
+        @foreach($articles as $article)
+            @include('articles.card')
+        @endforeach
+      </div>
     </div>
-    <div class="col-md-10 col-xs-12">
-      @foreach($articles as $article)
-          @include('articles.card')
-      @endforeach
-    </div>
+
 @endsection
--- laravel/resources/views/nav.blade.php
+++ laravel/resources/views/nav.blade.php
@@ -1,6 +1,7 @@
-<nav class="navbar navbar-expand navbar-dark blue-gradient">
+<nav class="mb-1 navbar navbar-expand-lg navbar-dark info-color">
 
-  <a class="navbar-brand" href="/"><i class="far fa-sticky-note mr-1"></i>memo</a>
+  <a class="navbar-brand" href="/"><i class="fas fa-chalkboard-teacher"></i> 掲示板SNS</a>
+ 
 
   <ul class="navbar-nav ml-auto">
     @guest
@@ -15,48 +16,54 @@
     @endguest
     @auth
     <li class="nav-item">
-    <a class="nav-link" href="{{ route('articles.create') }}"><i class="fas fa-pen mr-1"></i>投稿する</a> 
+    <a class="nav-link navbar-brand" href="{{ route('articles.create') }}"><i class="fas fa-pen mr-1"></i>記事を投稿</a> 
     </li>
     @endauth
-    @auth
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
-         aria-haspopup="true" aria-expanded="false">
-        @if(!empty(Auth::user()->image))
-              <img class='prof-photo' src="{{ asset('storage/images/'.Auth::user()->image) }}" >
-        @else
-              <i class="fas fa-user-circle fa-3x mr-1"></i>
-        @endif
-      </a>
-      <div class="dropdown-menu dropdown-menu-right dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
-        <button class="dropdown-item" type="button"
-                onclick="location.href='{{ route("users.show", ["name" => Auth::user()->name]) }}'">
-          マイページ
-        </button>
-        <div class="dropdown-divider"></div>
-        <button class="dropdown-item" type="button"
-                onclick="location.href='{{ route("users.edit", ["name" => Auth::user()->name]) }}'">
-          基本プロフィールを編集
-        </button>
-        <div class="dropdown-divider"></div>
-        <button class="dropdown-item" type="button"
-                onclick="location.href='{{ route("background") }}'">
-          経歴・実績を編集
-        </button>
-        <div class="dropdown-divider"></div>
-        <button class="dropdown-item" type="button"
-                onclick="location.href='{{ route("message_list", ["name" => Auth::user()->name]) }}'">
-          メッセージ一覧
-        </button>
-        <div class="dropdown-divider"></div>
-        <button form="logout-button" class="dropdown-item" type="submit">
-          ログアウト
-        </button>
-      </div>
-    </li>
+  </ul>
+  @auth
+  <div class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+        aria-haspopup="true" aria-expanded="false">
+      @if(!empty(Auth::user()->image))
+            <img class='prof-photo' src="{{ asset('storage/images/'.Auth::user()->image) }}" >
+      @else
+            <i class="fas fa-user-circle fa-3x mr-1"></i>
+      @endif
+    </a>
+    <div class="dropdown-menu dropdown-menu-right dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+      <button class="dropdown-item" type="button"
+              onclick="location.href='{{ route("articles.index") }}'">
+        トップ
+      </button>
+      <div class="dropdown-divider"></div>
+      <button class="dropdown-item" type="button"
+              onclick="location.href='{{ route("users.show", ["name" => Auth::user()->name]) }}'">
+        マイページ
+      </button>
+      <div class="dropdown-divider"></div>
+      <button class="dropdown-item" type="button"
+              onclick="location.href='{{ route("users.edit", ["name" => Auth::user()->name]) }}'">
+        基本プロフィールを編集
+      </button>
+      <div class="dropdown-divider"></div>
+      <button class="dropdown-item" type="button"
+              onclick="location.href='{{ route("background") }}'">
+        経歴・実績を編集
+      </button>
+      <div class="dropdown-divider"></div>
+      <button class="dropdown-item" type="button"
+              onclick="location.href='{{ route("message_list", ["name" => Auth::user()->name]) }}'">
+        メッセージ一覧
+      </button>
+      <div class="dropdown-divider"></div>
+      <button form="logout-button" class="dropdown-item" type="submit">
+        ログアウト
+      </button>
+    </div>
+  </div>
     <form id="logout-button" method="POST" action="{{ route('logout') }}">
     @csrf
     </form>
     @endauth
-  </ul>
+  
 </nav>
